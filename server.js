60e74037437659a0f50c26283ca7f8f4df05b159
Street Coder Nate <natebaker2427@gmail.com>
2021-06-28 05:53:14 -0500

    finished the backend service...ready for UI

--- server.js
+++ server.js
@@ -1,15 +1,21 @@
 const express = require("express");
 const { connectToDB } = require("./config/db_connection");
 const routes = require("./routes/landing");
+const api_routes = require("./routes/apis/landing");
+const bodyParser = require("body-parser");
 // To load env vars so that i dont have to put creds into the code
 require("dotenv").config();
+const path = require("path");
 
 // create the app
 const app = express();
 const PORT = process.env.PORT || 54321;
 
 // tell the app to check the routes on each connection
+app.use(bodyParser.json());
 app.use(routes);
+app.use(api_routes);
+app.use(express.static(path.join(__dirname, "public")));
 
 // connect to the mongo client and after connecting, print message and start the webserver
 connectToDB((err) => {
